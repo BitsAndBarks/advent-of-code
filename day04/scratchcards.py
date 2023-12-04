1c5c42b9dbf89457b8b083c4bafc00b5f71e99c2
BitsAndBarks <142544882+BitsAndBarks@users.noreply.github.com>
2023-12-04 23:24:44 +0100

    Solution for part 2.

--- day04/scratchcards.py
+++ day04/scratchcards.py
@@ -26,8 +26,8 @@ def parse_file_line(line):
     return int(game_id.split()[1]), number_sets
 
 
-def my_number_is_winning_number():
-
+# Checks all elements in second array if they also appear in first array. If yes, they're a winning number.
+def is_my_number_winning_number():
     for key, value in card_dict.items():
         winning_numbers, my_numbers = value
         matches = [element for element in my_numbers if element in winning_numbers]
@@ -36,6 +36,7 @@ def my_number_is_winning_number():
     return matching_elements
 
 
+# part 1
 def calculate_worth_of_pile():
     element_counts = {}
     sum_of_card_worth = 0
@@ -47,9 +48,50 @@ def calculate_worth_of_pile():
             sum_of_card_worth += 0
         element_counts[key] = count
 
-    print(sum_of_card_worth)
+    return sum_of_card_worth
 
 
-read_scratchcards_from_file(scratchcard_file)
-my_number_is_winning_number()
-calculate_worth_of_pile()
+# part 2
+def calculate_total_scratchcards():
+    total_cards = {key: 1 for key in card_dict}  # Start with 1 instance of each card
+    cards_to_process = total_cards.copy()  # Cards to process in the current iteration
+
+    while cards_to_process:
+        new_cards = {key: 0 for key in card_dict}  # Temporary storage for newly won cards
+
+        for key, count in cards_to_process.items():
+            if key not in matching_elements or not matching_elements[key]:  # Skip if no matches
+                continue
+
+            match_count = len(matching_elements[key])
+
+            for i in range(1, match_count + 1):
+                next_card = key + i
+                if next_card in total_cards:
+                    new_cards[next_card] += count  # Win new cards based on the count of the current card
+
+        # Update total_cards with the new cards won
+        for key, count in new_cards.items():
+            total_cards[key] += count
+
+        # Set up cards for the next iteration
+        cards_to_process = {k: v for k, v in new_cards.items() if v > 0}
+
+    return sum(total_cards.values())
+
+
+def main():
+    read_scratchcards_from_file(scratchcard_file)
+    is_my_number_winning_number()
+
+    # part 1
+    pile_worth = calculate_worth_of_pile()
+    print("Scratchcards are worth in total: ", pile_worth)
+
+    # part 2
+    total_scratchcards = calculate_total_scratchcards()
+    print("Total amount of received scratchcards:", total_scratchcards)
+
+
+if __name__ == "__main__":
+    main()
